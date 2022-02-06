11e688a0da588b076e34d140d21353217dd0c3f3
matan-ysayas <matan.ysayas@gmail.com>
2022-02-06 11:08:53 +0200

    mui

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "../components/Home/Home";
+import Products from "../components/Products/Products";
+import Orders from "../components/Orders/Orders";
+import CreateOrder from "../components/CreateOrder/CreateOrder";
+import CreateConnection  from "../components/CreateConnection/CreateConnection";
+import Header from "../components/Header/Header"
+import Sidebar from "../components/Sidebar/Sidebar"
+import Footer from "../components/Footer/Footer";
+
+
+const AppRouter =()=>{
+return(
+    <BrowserRouter>
+<Header/>
+<Sidebar/>
+<Routes>
+<Route exact path="/" element={<Home />}/>
+<Route exact path="/Products" element={<Products/>}/>
+<Route exact path="/Orders" element={<Orders/>}/>
+<Route exact path="/CreateOrder" element={<CreateOrder />}/>
+<Route exact path="/CreateConnection" element={<CreateConnection />}/> 
+</Routes>
+<Footer/>
+</BrowserRouter>
+)
+
+    
+}
+export default AppRouter;   
---
+++ src/components/ContextProvider.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createContext,useState } from "react";
+export const AgentContext=React.createContext();
+const ContextProvider =({Children})=>{
+
+    const [Agent,setAgent]=useState({AgentName:"" ,NumOfOrders:0,Email:"" })
+
+
+    return(
+
+<AgentContext.Provider value={Agent,setAgent}>
+{Children}
+</AgentContext.Provider>
+
+    )
+}
+export default ContextProvider;
